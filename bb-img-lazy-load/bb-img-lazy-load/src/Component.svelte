b982bf356663194a3ce069fe270de7191571c9a6
Ash Carter <ash@carter.ac>
2023-08-16 09:24:23 -0400

    lazy image component

---
+++ bb-img-lazy-load/bb-img-lazy-load/src/Component.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { getContext } from "svelte"
+  import { useLazyImage as lazyImage } from 'svelte-lazy-image';
+
+  export let imgSrc
+  export let imgAltText = "Assessment Capture"
+
+  const { styleable } = getContext("sdk")
+  const component = getContext("component")
+</script>
+
+<img use:styleable={$component.styles} data-src={imgSrc} alt={imgAltText} use:lazyImage />
